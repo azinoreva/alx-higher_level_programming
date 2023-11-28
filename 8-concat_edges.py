91ec8c75ac79e01a1c15488fbee58ecaedc3457d
azinoreva <azinoreva@gmail.com>
2023-11-27 16:12:01 -0800

    Check if on track

--- 8-concat_edges.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = f"{str[38:66]} with {str[0:6]}\n"
-print(str)
--- README.md
+++
@@ -1,1 +0,0 @@
-THis is my first python work on ALX. The tasks are mostly about strings and running python on unix shell.
