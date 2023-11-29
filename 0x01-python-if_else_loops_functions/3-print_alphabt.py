ec38a54e3cd040ef1ce6bc0a04e497ef267583d5
azinoreva <azinoreva@gmail.com>
2023-11-28 20:49:31 -0800

    complete

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
+for num in range(97, 123):
     if (num == 101) or (num == 113):
         continue
-    print(chr(i).format(), end="")
+    print(chr(num).format(), end="")
