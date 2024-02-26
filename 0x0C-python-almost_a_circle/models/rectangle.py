a311803e659f2124f1965a4549b0a46c308c7975
azinoreva <azinoreva@gmail.com>
2024-02-26 12:12:25 -0800

    Redone

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
 """ Importing important modules and libraries """
-import base
-from base import Base
+from models.base import Base
 
-""" Rectangle class """
+
 class Rectangle(Base):
+    """ Rectangle class """
 
-    """ initialized the function that represents """
     def __init__(self, width, height, x=0, y=0, id=None): 
+        """ initialized a new rectangle
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         self.__width = width
         self.__height= height
         self.__x = x
         self.__y = y
-        """ taking id from base """
+        
         super().__init__(id)
 
-    """ making width accessible """
     @property
     def width(self):
+        """ making width accessible """
         return self.__width
     
-    """ defining and setting the width """
     @width.setter
     def width (self, value):
+        """ defining and setting the width """
         if not isinstance(value,int):
             raise TypeError("width must be an integer")
         if value <= 0: 
@@ -30,14 +42,14 @@ class Rectangle(Base):
         else:
             self.__width = value
 
-    """ making height accessible """   
     @property
     def height(self):
+        """ making height accessible """
         return self.__height
     
-    """ defining and setting the height """
     @height.setter
     def height (self, value):
+        """ defining and setting the height """
         if not isinstance(value,int):
             raise TypeError("height must be an integer")
         if value <= 0: 
@@ -45,14 +57,14 @@ class Rectangle(Base):
         else:
             self.__height = value
     
-    """ making x accessible """ 
     @property
     def x(self):
+        """ making x accessible """
         return self.__x
     
-    """ defining and setting x """
     @x.setter
     def x (self, value):
+        """ defining and setting x """
         if not isinstance(value,int):
             raise TypeError("x must be an integer")
         if value <= 0: 
@@ -60,14 +72,14 @@ class Rectangle(Base):
         else:
             self.__x = value
 
-    """ making y accessible """
     @property
     def y(self):
+        """ making y accessible """
         return self.__y
     
-    """ defining and setting y """
     @y.setter
     def y (self, value):
+        """ defining and setting y """
         if not isinstance(value,int):
             raise TypeError("y must be an integer")
         if value <= 0: 
@@ -75,23 +87,23 @@ class Rectangle(Base):
         else:
             self.__y = value
 
-    """ Calculate and return the area of the rectangle """ 
     def area(self):
+        """ Calculate and return the area of the rectangle """
         return (self.width * self.height)
    
-    """ Display the rectangle using '#' characters """ 
     def display(self):
+        """ Display the rectangle using '#' characters """
         for i in range(self.height):
             print('#' * self.width)
             i += 1 
 
-    """ Return a string representation of the rectangle object """ 
     def __str__(self):
+       """ Return a string representation of the rectangle object """
        class_name = self.__class__.__name__
        return f"[{class_name}]  ({self.id}) {self.__x}/{self.__y} - {self.width}/{self.height}"
     
-    """ Update attributes based on either positional or keyword arguments """
     def update(self, *args, **kwargs):
+        """ Update attributes based on either positional or keyword arguments """
         if len(args) > 0:
             # If positional arguments exist, update attributes in the order they are defined
             attributes = ['id', 'width', 'height', 'x', 'y']
